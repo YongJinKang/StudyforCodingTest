57b0fc24b063e769fdee47d282810dc7f6a20824
YongJinKang <kyongjin1234@gmail.com>
2021-01-13 00:01:46 +0900

    5.py

--- 백준/if/5.py
+++ 백준/if/5.py
@@ -6,35 +6,28 @@
 # elif h < 1 and m >= 45:
 #     h = h
 #     m = m - 45
-# elif h > 1 and m < 45:
+# elif h >= 1 and m < 45:
 #     h = h - 1
 #     m = m + 15
-# elif h > 1 and m >= 45:
+# elif h >= 1 and m >= 45:
 #     h = h
 #     m = m - 45
 # print(h,m)
 
+
 h,m = map(int,input().split())
 
-if h < 1 and m < 45:
-    h = 23
-    m = m + 15
-elif h < 1 and m >= 45:
-    h = h
-    m = m - 45
-elif h >= 1 and m < 45:
-    h = h - 1
-    m = m + 15
-elif h >= 1 and m >= 45:
-    h = h
-    m = m - 45
-print(h,m)
 
-if m < 45:
-    m = m + 15
-    if h < 1:
+if h < 1:
+    if m < 45:
         h = 23
+        m += 15
     else:
-        h = h - 1
+        m -= 45
 else:
-    m = m - 45
+    if m < 45:
+        h -= 1
+        m += 15
+    else:
+        m -= 45
+print(h,m)
